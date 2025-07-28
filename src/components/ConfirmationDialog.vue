850831b0fe981a211a5d4dac4aca0dd3607e16a1
Anuj Jhawar <anujjhawar28@gmail.com>
2025-07-29 01:06:14 +0530

    Fixed the width for Delete Popup

--- src/components/ConfirmationDialog.vue
+++ src/components/ConfirmationDialog.vue
@@ -5,26 +5,30 @@
     :closable="false"
     :dismissableMask="true"
     header="Please Confirm"
-    style="width: 30vw"
+    class="!mx-auto"
+    :style="{ width: '100%', maxWidth: '500px' }"
+    :breakpoints="{ '640px': '95vw' }"
     @update:visible="$emit('update:modelValue', false)"
   >
-    <p class="text-lg mb-4">
+    <p class="text-lg mb-4 text-center">
       {{ message }}
     </p>
 
     <template #footer>
-      <Button
-        label="Cancel"
-        icon="pi pi-times"
-        class="p-button-text"
-        @click="$emit('update:modelValue', false); $emit('cancel')"
-      />
-      <Button
-        label="Confirm"
-        icon="pi pi-check"
-        class="p-button-danger"
-        @click="$emit('confirm')"
-      />
+      <div class="flex justify-end gap-2">
+        <Button
+          label="Cancel"
+          icon="pi pi-times"
+          class="p-button-text"
+          @click="$emit('update:modelValue', false); $emit('cancel')"
+        />
+        <Button
+          label="Confirm"
+          icon="pi pi-check"
+          class="p-button-danger"
+          @click="$emit('confirm')"
+        />
+      </div>
     </template>
   </Dialog>
 </template>
